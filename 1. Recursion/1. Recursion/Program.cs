32f0ad3e26fe7efb1e2d39643d838de285cf11c1
Omer Yumer <y.yumer@gmail.com>
2015-09-27 22:02:09 +0200

    Problem 1. & 2. - solved

--- 1. Recursion/1. Recursion/Program.cs
+++ 1. Recursion/1. Recursion/Program.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace _1.Recursion
+namespace T_01.Recursion
 {
     class Program
     {
+        private static int stepsTaken = 0;
+
+        private static Stack<int> source;
+        private static Stack<int> destination = new Stack<int>();
+        private static Stack<int> spare = new Stack<int>();
+
         static void Main(string[] args)
         {
+            int numberOfDiscs = 10;
+            source = new Stack<int>(Enumerable.Range(1, numberOfDiscs).Reverse());
+            PrintRods();
+            MoveDiscs(numberOfDiscs, source, destination, spare);
+        }
+
+        private static void MoveDiscs(int bottomDisc, Stack<int> source, Stack<int> destination, Stack<int> spare)
+        {
+            if (bottomDisc == 1)
+            {
+                stepsTaken++;                
+                destination.Push(source.Pop());
+                Console.WriteLine("Step {0}: moved disk {1}", stepsTaken, bottomDisc);
+                PrintRods();
+            }
+            else
+            {
+                //todo
+                MoveDiscs(bottomDisc - 1, source, spare, destination);
+
+                stepsTaken++;                
+                destination.Push(source.Pop());
+                Console.WriteLine("Step {0}: moved disk {1}", stepsTaken, bottomDisc);
+                PrintRods();
+
+                //todo
+                MoveDiscs(bottomDisc - 1, spare, destination, source);
+            }
+        }
+
+        private static void PrintRods()
+        {
+            Console.WriteLine("Source: {0}", string.Join(", ", source.Reverse()));
+            Console.WriteLine("Destination: {0}", string.Join(", ", destination.Reverse()));
+            Console.WriteLine("Spare: {0}", string.Join(", ", spare.Reverse()));
+            Console.WriteLine();
         }
     }
 }
---
+++ 1. Recursion/T02_NestedLoopsRecursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T02_NestedLoopsRecursion
+{
+    class Program
+    {
+        static int numNestedLoops = 3;
+        static int[] arrNumbers = new int[numNestedLoops];
+
+        static void Main(string[] args)
+        {
+            NestedLoopToRecusrion(arrNumbers, 0);
+        }
+
+        static void NestedLoopToRecusrion(int[] arr, int index)
+        {
+            if (index >= arr.GetLength(0))
+            {
+                PrintArr();
+                return;
+            }
+            else
+            {
+                for (int i = 1; i <= numNestedLoops; i++)
+                {
+                    arr[index] = i;
+                    NestedLoopToRecusrion(arr, index + 1);
+                }
+            }
+        }
+
+        static void PrintArr()
+        {
+            for (int i = 0; i < arrNumbers.Length; i++)
+            {
+                Console.Write(arrNumbers[i] + " ");                
+            }
+            Console.WriteLine();
+        }
+    }
+}
